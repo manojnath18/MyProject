7e406d1df75e5b5a653d3661d4e29c680760edf7
manojnath18 <maxseezon15@gmail.com>
2025-06-14 13:15:28 +0545

    url changed

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://myproject-43og.onrender.com',
     credentials:true
 }
 
