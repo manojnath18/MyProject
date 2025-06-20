dd231b15e2e166f2a4493fe7ef5feb5ceb4baaeb
Manoj Nath <161053926+manojnath18@users.noreply.github.com>
2025-06-20 18:40:19 +0545

    Make responsive for mobile and all devices

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -64,9 +64,11 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId).populate({
-            path:"applications"
-        });
+        const job = await Job.findById(jobId).populate([
+            { path: "applications" },
+            { path: "company" }
+        ]);
+
         if (!job) {
             return res.status(404).json({
                 message: "Jobs not found.",
@@ -83,8 +85,8 @@ export const getAdminJobs = async (req, res) => {
     try {
         const adminId = req.id;
         const jobs = await Job.find({ created_by: adminId }).populate({
-            path:'company',
-            createdAt:-1
+            path: 'company',
+            createdAt: -1
         });
         if (!jobs) {
             return res.status(404).json({
